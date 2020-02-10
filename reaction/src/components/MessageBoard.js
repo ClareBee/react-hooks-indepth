fdfd7baabae2865acc875a4ed55ea5a220b1fe4e
Clare <chblackburne@gmail.com>
2020-02-10 10:42:47 +0000

    message reactions component

--- reaction/src/components/MessageBoard.js
+++ reaction/src/components/MessageBoard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useAppContext } from './hooks';
 import CreateReaction from './CreateReaction';
+import MessageReactions from './MessageReactions';
 
 function MessageBoard(){
   // inner destructuring
-  const { state: { messages } } = useAppContext();
+  const { state: { messages, reactionsMap } } = useAppContext();
   return(
     <div>
     {messages.map(message => {
@@ -15,6 +16,7 @@ function MessageBoard(){
           <p>{text}</p>
           <h4>- {username}</h4>
           <CreateReaction messageId={id}/>
+          <MessageReactions messageReactions={reactionsMap[id]} />
           <hr />
         </div>
       )
